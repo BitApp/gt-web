f2c65bb49711bacbd0cad98caaedabe385795da8
AlanGuo <AlanGuo>
2020-03-03 09:35:07 +0800

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,128 @@
 <template>
   <div class="gt-body">
-    <div class="gt-web-index">
-      <div>
+    <div class="gt-web-index clearfix">
+      <div class="fr">
         <b-form-select v-model="language" :options="langs" @change="changeLang"></b-form-select>
       </div>
-      <div class="banner-bg mt-10">
-        <div class="banner">
-          <div class="title">清风链游戏公益积分 GT（GUILD_TOKEN)</div>
-          <div class="banner-content d-flex">
-            <div class="banner-content-left">
-              <div class="mt-8">
-                <div>1. 总量 21亿 / 2. 超级回购计划</div>
-                <div>3. 每年减半 / 4. 可随时兑换 IOST</div>
-                <div>5. 积分商城即将上线</div>
+      <b-tabs pills>
+        <b-tab title="投票" active>
+          <div class="vote-web-view">
+            <div class="banner-bg mt-10">
+              <div class="banner">
+                <div class="title">清风链游戏公益积分 GT（GUILD_TOKEN)</div>
+                <div class="banner-content d-flex">
+                  <div class="banner-content-left">
+                    <div class="mt-8">
+                      <div>1. 总量 21亿 / 2. 超级回购计划</div>
+                      <div>3. 每年减半 / 4. 可随时兑换 IOST</div>
+                      <div>5. 积分商城即将上线</div>
+                    </div>
+                  </div>
+                  <img class="icon-gt" src="~/assets/imgs/icon_gt.svg" width="75">
+                </div>
               </div>
             </div>
-            <img class="icon-gt" src="~/assets/imgs/icon_gt.svg" width="75">
+            <div class="countdown mt-15">
+              <div class="countdown-content d-flex">
+                <div>减半倒计时：</div>
+                <no-ssr>
+                <countdown class="number" :time="(nextHalve-currentBlock) * 500">
+                  <template slot-scope="props">{{ props.days }} 天{{ props.hours }} 小时{{ props.minutes }} 分{{ props.seconds }} 秒</template>
+                </countdown>
+                </no-ssr>
+              </div>
+            </div>
+            <div class="mt-15 info-view">
+              <div class="info-line" @click="voteNumber = fixedNumber(accountInfo.balance,6)">我的IOST：{{fixedNumber(accountInfo.balance,6)}}</div>
+              <div class="info-line">投票中的IOST：{{votebalances}}</div>
+              <div class="frozen-line">
+                <span>冻结中的IOST：{{frozenbalances}}</span>
+                <b-link @click="unvoteModal">马上赎回</b-link>
+              </div>
+            </div>
+            <div class="exchange-info mt-20">
+              <div class="icon-view">
+                <img class="icon-img" src="@/assets/imgs/icon_token.svg">
+                <img class="icon-to" src="@/assets/imgs/icon_to.svg">
+                <img class="icon-img" src="@/assets/imgs/icon_gt.svg">
+              </div>
+              <div class="font-norwester fs-20 scale-title mt-20">
+                1 GT = 0.066 IOST
+              </div>
+              <div class="scale-desc">投票给 hibtc 节点即可免费获得 GT</div>
+              <b-input-group>
+                <b-form-input type="number" v-model="voteNumber" placeholder="请输入投票数量" @update="inputChange" autocomplete="off"></b-form-input>
+                <b-input-group-append>
+                  <div class="all-btn" @click="voteNumber = fixedNumber(accountInfo.balance,0);inputChange()">全部</div>
+                </b-input-group-append>
+              </b-input-group>
+              <div style="padding:10px">
+                <div class="scale-tip">当前hibtc总票数{{'\xa0'+parseInt(producerVotes) + '\xa0'}}, 投 {{'\xa0'+(voteNumber || 0)+'\xa0'}} IOST 给 hibtc，每天可分得 {{'\xa0'+fixedNumber(abctNumber,6) + '\xa0'}} hibtc </div> 
+                <div class="scale-tip mt-2 fb">{{fixedNumber(abctNumber,6) + '\xa0'}} hibtc = {{'\xa0'+fixedNumber(iostNumber,6) + '\xa0'}} IOST = {{fixedNumber(priceNumber,6) + (/cn/i.test(lang.lang)?' CNY':' USD')}}</div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="countdown mt-15">
-        <div class="countdown-content d-flex">
-          <div>减半倒计时：</div>
-          <no-ssr>
-          <countdown class="number" :time="(nextHalve-currentBlock) * 500">
-            <template slot-scope="props">{{ props.days }} 天{{ props.hours }} 小时{{ props.minutes }} 分{{ props.seconds }} 秒</template>
-          </countdown>
-          </no-ssr>
-        </div>
-      </div>
-      <div class="exchange mt-20">
-        <div class="exchange-tip">
-          <div class="abct-text d-flex">
-            <div class="fb">
-              我的GT：{{tokenbalance}}
+        </b-tab>
+        <b-tab title="回购">
+          <div class="exchange-web-view mt-20">
+            <div class="banner-bg mt-10">
+              <div class="banner">
+                <div class="title">清风链游戏公益积分 GT（GUILD_TOKEN)</div>
+                <div class="banner-content d-flex">
+                  <div class="banner-content-left">
+                    <div class="mt-8">
+                      <div>1. 总量 21亿 / 2. 超级回购计划</div>
+                      <div>3. 每年减半 / 4. 可随时兑换 IOST</div>
+                      <div>5. 积分商城即将上线</div>
+                    </div>
+                  </div>
+                  <img class="icon-gt" src="~/assets/imgs/icon_gt.svg" width="75">
+                </div>
+              </div>
+            </div>
+            <div class="exchange-tip">
+              <div class="abct-text d-flex">
+                <div class="fb">
+                  我的GT：{{tokenbalance}}
+                </div>
+                <div>
+                  <b-btn variant="link" size="sm" @click="exchangeNumber = tokenbalance">全部</b-btn>
+                </div>
+              </div>
+              <div class="exchange-pool">
+                <b-input-group>
+                  <b-form-input focus type="number" min=1 v-model="exchangeNumber" step="1" placeholder="请输入兑换数量" autocomplete="off"></b-form-input>
+                  <b-input-group-text><strong>0.066 IOST / GT</strong></b-input-group-text>
+                </b-input-group>
+                <div class="mt-20">
+                  <b-btn block size="lg" variant="primary" @click="exchange">兑换</b-btn>
+                </div>
+                <div class="mt-15 ta">
+                  <b-btn variant="link" @click="historyModal">查看兑换记录 ></b-btn>
+                </div>
+              </div>
+              <div class="exchange-pool mt-15">
+                <span>兑换资金池：{{fixedNumber(contractBalance.balance,2)+ '\xa0 IOST'}}  </span>
+                <img class="icon_largerise" src="~/assets/imgs/icon_largerise2.svg">
+              </div>
+              <div class="exchange-pool mt-15">
+                <span>总兑换：{{fixedNumber(totalExchange, 2)+ '\xa0 GT' + '\xa0 = \xa0' + fixedNumber(totalExchange * 0.066,2) + '\xa0 IOST'}}  </span>
+              </div>
             </div>
             <div>
-              <b-btn variant="link" size="sm" @click="exchangeNumber = tokenbalance">全部</b-btn>
+              <p class="mt-20">说明:</p>
+              <p class="mt-10">1. 清风链游戏公益积分 GUILD_TOKEN，简称GT总发行量21亿</p>
+              <p>2. 每天固定发行5万Token，按照投票用户的投票占比清风链节点总得票数的比例分得</p>
+              <p>3. 每过一年，发行量将减少一半</p>
+              <p>4. 清风链官方将每月固定配发5万IOST以0.066 IOST/GT 的汇率回购GT代币</p>
+              <p>5. 清风链将在未来推出积分商城，大家可以使用GT兑换商城里的任意物品，包括实物和虚拟物品等</p>
+              <p>6. 本活动最终解释权归清风链游戏公社所有</p>
             </div>
           </div>
-          <div class="exchange-pool">
-            <b-input-group>
-              <b-form-input focus type="number" min=1 v-model="exchangeNumber" step="1" placeholder="请输入兑换数量" autocomplete="off"></b-form-input>
-              <b-input-group-text><strong>0.066 IOST / GT</strong></b-input-group-text>
-            </b-input-group>
-            <div class="mt-20">
-              <b-btn block size="lg" variant="primary" @click="exchange">兑换</b-btn>
-            </div>
-            <div class="mt-15 ta">
-              <b-btn variant="link" @click="historyModal">查看兑换记录 ></b-btn>
-            </div>
-          </div>
-          <div class="exchange-pool mt-15">
-            <span>兑换资金池：{{fixedNumber(contractBalance.balance,2)+ '\xa0 IOST'}}  </span>
-            <img class="icon_largerise" src="~/assets/imgs/icon_largerise2.svg">
-          </div>
-          <div class="exchange-pool mt-15">
-            <span>总兑换：{{fixedNumber(totalExchange, 2)+ '\xa0 GT' + '\xa0 = \xa0' + fixedNumber(totalExchange * 0.066,2) + '\xa0 IOST'}}  </span>
-          </div>
-        </div>
-        <div>
-          <p class="mt-20">说明:</p>
-          <p class="mt-10">1. 清风链游戏公益积分 GUILD_TOKEN，简称GT总发行量21亿</p>
-          <p>2. 每天固定发行5万Token，按照投票用户的投票占比清风链节点总得票数的比例分得</p>
-          <p>3. 每过一年，发行量将减少一半</p>
-          <p>4. 清风链官方将每月固定配发5万IOST以0.066 IOST/GT 的汇率回购GT代币</p>
-          <p>5. 清风链将在未来推出积分商城，大家可以使用GT兑换商城里的任意物品，包括实物和虚拟物品等</p>
-          <p>6. 本活动最终解释权归清风链游戏公社所有</p>
-        </div>
-      </div>
-      <HistoryModal ref="historyModal" />
+          <HistoryModal ref="historyModal" />
+        </b-tab>
+        <b-tab title="商城"><p class="mt-20 ta-c">商城即将上线，敬请期待</p></b-tab>
+      </b-tabs>
     </div>
   </div>
 </template>
@@ -253,11 +306,9 @@ export default {
   font-size: 14px;
   .gt-web-index {
     padding: 30px 20px;
-    
     .mt-8 {
       margin-top: 8px;
     }
-    // box-shadow: #111 1px 1px 0;
     .banner{
       border-radius: 8px;
       background: #2A3657;
@@ -296,28 +347,75 @@ export default {
         }
       }
     }
-    .tip-view {
-      display: flex;
-      .qrcode{
-        flex: 1;
-        padding: 10px;
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-end;
-        .code-img{
-          text-align: left;
+    .vote-web-view{
+      .info-view{
+        padding: 15px;
+        background: #2A3657;
+        .info-line{
+          line-height: 25px;
+        }
+        .frozen-line{
+          display: flex;
+          justify-content: space-between;
         }
       }
+      .exchange-info{
+        padding: 15px;
+        background: #2A3657;
+        .icon-view{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          .icon-img{
+            height: 40px;
+          }
+          .icon-to{
+            margin: 0 10px;
+            height: 15px;
+          }
+        }
+        #price{
+          display: inline-block;
+          width: 140px;
+        }
+        .switch{
+          margin-left:5px;
+        }
+        .scale-title{
+          text-align: center;
+        }
+        .scale-desc{
+          text-align: center;
+          padding: 20px;
+        }
+        .scale-tip{
+          line-height: 20px;
+          color: rgba(255, 255, 255, 0.6);
+        }
+      }
+      .mask-view{
+        position: absolute;
+        left: 0;
+        top: 0;
+        background-color: rgba(0, 0, 0, 0.6);
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
     }
-    .tips-view{
-      display: flex;
-      justify-content: space-between;
-      color: rgba(255, 255, 255, 0.7);
-    }
-    .exchange{
+    .exchange-web-view {
+      .banner{
+        border-radius: 8px;
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+      }
       .exchange-tip{
         padding: 20px;
         border-radius: 8px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
         background-color:#2A3657;
         .exchange-pool{
           .icon_largerise{
@@ -331,17 +429,6 @@ export default {
           align-items: center;
         }
       }
-    }
-    .mask-view{
-      position: absolute;
-      left: 0;
-      top: 0;
-      background-color: rgba(0, 0, 0, 0.6);
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
     }
   }
 }
